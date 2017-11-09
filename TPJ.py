0946eea55d7702d64160ff17bd6acc262c976f9e
flummingbird <wwconlin@gmail.com>
2017-11-09 15:34:48 -0600

    initial commit

---
+++ TPJ.py
@@ -0,0 +1,23 @@
+import json
+from pprint import pprint
+
+
+with open('bounce-titles') as f:
+    title_list = f.read().splitlines()
+
+with open('solr-out.json', 'r', encoding='utf-8') as s:
+    solr_out =  s.read()
+    data_dict = json.loads(solr_out)
+
+
+for i in data_dict["response"]["docs"]:
+    if title_list[0] == i['dc.title'][0]: 
+        print(i["PID"])
+    
+
+
+# print(title_list[0])
+# print(data_dict["response"]["docs"][0]["dc.title"][0])
+
+
+
