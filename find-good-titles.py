4ff531be76912eb054c8dab655cc02b84618d540
flummingbird <wwconlin@gmail.com>
2017-11-10 11:09:03 -0600

    fix duplicates in bad pid logic, got a more complete titlelist

--- find-good-titles.py
+++ find-good-titles.py
@@ -10,12 +10,18 @@ with open('solr-out.json', 'r', encoding='utf-8') as s:
     data_dict = json.loads(solr_out)
 
 
+
 for x in title_list:
     for i in data_dict["response"]["docs"]:
-        if x == i['dc.title'][0]: 
+        if x == i['dc.title'][0] or x + ':' == i['dc.title'][0]:
             print(i["PID"])
+
+
+    # if x not in data_dict and x + ':' not in data_dict:
+    #     print(x + ' not found')
     
 
+#didn't work perfectly see bounce titles many missed \\" ect \( \)
 
 # print(title_list[0])
 # print(data_dict["response"]["docs"][0]["dc.title"][0])
--- swap-good-for-bad-pids.py
+++ swap-good-for-bad-pids.py
@@ -1,8 +1,23 @@
 import json
 
-with open('good-bounce-pids') as f:
-    title_list = f.read().splitlines()
+with open('good-bounce-pids.txt') as f:
+    g_pid_list = f.read().splitlines()
 
 with open('solr-out.json', 'r', encoding='utf-8') as s:
     solr_out =  s.read()
     data_dict = json.loads(solr_out)
+
+bad_pids = []
+
+
+for i in data_dict["response"]["docs"]:
+    if i["PID"] in g_pid_list:
+        continue
+    else:  
+        bad_pids.append(i["PID"])
+
+
+bads = list(set(bad_pids))
+
+for z in bads:
+    print(z)
