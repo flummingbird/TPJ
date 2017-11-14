80962f2d9731ee0122bd2a4a66ed0ddc2ca27d43
flummingbird <wwconlin@gmail.com>
2017-11-14 15:45:36 -0600

    renames, var- fixes etc

--- swap-pids.py
+++ swap-pids.py
@@ -4,21 +4,21 @@ pids_input = input("pidlist filename?: ")
 
 solr_file = input("solr_json to strip pids_input out of?: ")
 
-with open('files/good-bounce-pids.txt') as f:
-    g_pid_list = f.read().splitlines()
+with open('files/'+ pids_input) as f:
+    pid_list = f.read().splitlines()
 
-with open('files/solr-out.json', 'r', encoding='utf-8') as s:
+with open('files/' + solr_file, 'r', encoding='utf-8') as s:
     solr_out =  s.read()
     data_dict = json.loads(solr_out)
 
-bad_pids = []
+remaining_pids = []
 
 
 for i in data_dict["response"]["docs"]:
-    if i["PID"] in g_pid_list:
+    if i["PID"] in pid_list:
         continue
     else:  
-        bad_pids.append(i["PID"])
+        remaining_pids.append(i["PID"])
 
 
 bads = list(set(bad_pids))
